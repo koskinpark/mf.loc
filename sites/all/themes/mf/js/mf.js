551afcbd9bc818135498936b0a56e34c15b00eb0
Konstantin Shupenko <koskinparkin@gmail.com>
2015-04-10 18:15:12 +0600

    commit from 10.04

--- sites/all/themes/mf/js/mf.js
+++ sites/all/themes/mf/js/mf.js
@@ -20,4 +20,26 @@
             });
         }
     };
+    Drupal.behaviors.seemorebutton = {
+        attach: function (context, settings) {
+            $( '.pane-see-more-product-homepage-panel-pane-1' ).prepend( "<div class='see-more'><span class='see-more-btn'>See More</span><img src='sites/all/themes/mf/images/arrow_see_more.png'></div>" );
+            $(".see-more-btn").hover(function () {
+                $(".see-more").addClass("see-more-hover");
+                $(".view-id-see_more_product_homepage").addClass('see-more-pictures');
+            });
+            $(".see-more-btn").mouseout(function () {
+                $(".see-more").removeClass("see-more-hover");
+            });
+            $(".see-more-btn").click(function () {
+                $(".pane-see-more-product-homepage-panel-pane-1").addClass("see-more-visible");
+            });
+
+            $(".pane-see-more-product-homepage-panel-pane-1").hover(function () {
+                $(".view-id-see_more_product_homepage").addClass('see-more-pictures');
+            });
+            $(".pane-see-more-product-homepage-panel-pane-1").mouseout(function () {
+                $(".view-id-see_more_product_homepage").removeClass('see-more-pictures');
+            });
+        }
+    };
 })(jQuery);
