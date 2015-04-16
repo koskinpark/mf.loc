b8ff7fd17b17fc710113c38e9b9e2634a158daf6
Konstantin Shupenko <koskinparkin@gmail.com>
2015-04-16 18:23:33 +0600

    commit from 16.04

--- sites/all/themes/mf/js/mf.js
+++ sites/all/themes/mf/js/mf.js
@@ -5,9 +5,40 @@
     Drupal.behaviors.colorboxmenu = {
         attach: function (context, settings) {
 
+            $("#cboxClose").empty();
+            $("#cboxClose").hover(function () {
+                $("#cboxClose").css( {'opacity':'1', 'transition':'0.3s'});
+            });
+            $("#cboxClose").mouseout(function () {
+                $("#cboxClose").css( {'opacity':'0.2', 'transition':'0.3s'});
+            });
+
+            function margins($default_width, $default_height, relativeX, relativeY) {
+                var margins = {};
+                var $half_width = $default_width/2;
+                var $half_height = $default_height/2;
+                var $margin_horisont = 2*($half_width - relativeX);
+                var $margin_vertical = 2*($half_height - relativeY);
+
+                if ($margin_horisont > 0) {
+                    margins.margin_right = -($margin_horisont);
+                }
+                else {
+                    margins.margin_left = $margin_horisont;
+                }
+
+                if ($margin_vertical > 0) {
+                    margins.margin_bottom = -($margin_vertical);
+                }
+                else {
+                    margins.margin_top = $margin_vertical;
+                }
+                return margins;
+            }
+
             $('.node--product--colorbox', context).once('colorbox-imageslide', function() {
                 var $colorbox_node_wrapper = $(this);
-                $($colorbox_node_wrapper).append("<div class='change-image'><span class='increase'></span><span class='decrease'></span></div>");
+                $($colorbox_node_wrapper).append("<div class='change-image'><span>Zoom</span><span class='increase'></span><span class='decrease'></span></div>");
                 var $big_pictures_items = $('> .field--name-field-pictures .field__items', $colorbox_node_wrapper);
                 var $big_pictures = $('.field__item', $big_pictures_items);
                 var $thumbnails = $('> .field--name-one-more-picture .field--name-field-pictures .field__item', $colorbox_node_wrapper);
@@ -15,12 +46,9 @@
 
                 $default_width = $('img', $big_pictures[0]).attr('width');
                 $default_height = $('img', $big_pictures[0]).attr('height');
-                $factor = $default_width / $default_height;
-                $height = 2*$default_height;
-                $width = $height*$factor;
                 var $checkpoint = false;
                 $($big_pictures[0]).addClass('visible');
-                $('img', $big_pictures[0]).css({'width': $default_width, 'height': $default_height, 'transition': '1s'});
+                $('img', $big_pictures[0]).css({'transform': 'scale(1)', 'transition': '0.5s'});
                 $thumbnails.each(function(i) {
                     var $thumbnail = $(this);
                     $thumbnail.click(function() {
@@ -33,12 +61,10 @@
                                         $('img', $big_pictures).removeClass('increased');
                                     }
                                     $($big_pictures[i]).addClass('visible');
+                                    $change_imgclick = imgClickif;
                                     $default_width = $('img', $big_pictures[i]).attr('width');
                                     $default_height = $('img', $big_pictures[i]).attr('height');
-                                    $factor = $default_width / $default_height;
-                                    $height = 2*$default_height;
-                                    $width = $height*$factor;
-                                    $('img', $big_pictures[i]).css({'width': $default_width, 'height': $default_height, 'transition': '1s'});
+                                    $('img', $big_pictures[i]).css({'transform': 'scale(1)', 'transition': '0.5s'});
                                 });
                                 $big_pictures_items.fadeIn(300, function () {
                                     $checkpoint = false;
@@ -46,29 +72,58 @@
                             }
                         }
                     });
-                    $('img', $big_pictures[i]).click(function() {
+                    var imgClickif = function(relativeX, relativeY) {
                         if (!$('img', $big_pictures[i]).hasClass('increased')) {
                             $('img', $big_pictures[i]).addClass('increased');
-                            $('img', $big_pictures[i]).css({'width': $width, 'height': $height, 'transition': '1s'});
-                           // $('img', $big_pictures[i]).draggable();
+                          //  $new_sizes = resize($default_width, $default_height);
+                            $margins = margins($default_width, $default_height, relativeX, relativeY);
+
+                            $('img', $big_pictures[i]).css({
+                                'margin-left': $margins['margin_left'],
+                                'margin-top': $margins['margin_top'],
+                                'margin-bottom': $margins['margin_bottom'],
+                                'margin-right': $margins['margin_right'],
+                                'transform': 'scale(2)',
+                                'transition': '0.5s'});
 
                         }
-                        else {
-                            $('img', $big_pictures[i]).removeClass('increased');
-                            $('img', $big_pictures[i]).css({'width': $default_width, 'height': $default_height, 'transition': '1s'});
-                        }
-                    });
-                    $("> .change-image .increase", $colorbox_node_wrapper).click(function() {
-                        if (!$('img', $big_pictures[i]).hasClass('increased')) {
-                            $('img', $big_pictures[i]).addClass('increased');
-                            $('img', $big_pictures[i]).css({'width': $width, 'height': $height, 'transition': '1s'});
-                        }
-                    });
-                    $("> .change-image .decrease", $colorbox_node_wrapper).click(function() {
+                    };
+                    var imgClickelse = function() {
                         if ($('img', $big_pictures[i]).hasClass('increased')) {
-                            $('img', $big_pictures[i]   ).removeClass('increased');
-                            $('img', $big_pictures[i]).css({'width': $default_width, 'height': $default_height, 'transition': '1s'});
+                            $('img', $big_pictures[i]).removeClass('increased');
+                            $('img', $big_pictures[i]).css({
+                                'margin-left': '0',
+                                'margin-top': '0',
+                                'margin-right': '0',
+                                'margin-bottom': '0',
+                                'transform': 'scale(1)',
+                                'transition': '0.5s'});
                         }
+                    };
+                    var $change_imgclick = imgClickif;
+                    $('img', $big_pictures[i]).click(function(e) {
+                        if ($change_imgclick == imgClickif) {
+                            var offset = $('img', $big_pictures[i]).offset();
+                            var relativeX = (e.pageX - offset.left);
+                            var relativeY = (e.pageY - offset.top);
+                            imgClickif(relativeX, relativeY);
+
+                            $change_imgclick = imgClickelse;
+                        }
+                        else {
+                            imgClickelse();
+                            $change_imgclick = imgClickif;
+                        }
+                    });
+
+
+                    $("> .change-image .increase", $colorbox_node_wrapper).click(function(){
+                        imgClickif();
+                        $change_imgclick = imgClickelse;
+                    });
+                    $("> .change-image .decrease", $colorbox_node_wrapper).click(function(){
+                        imgClickelse();
+                        $change_imgclick = imgClickif;
                     });
                 });
 
@@ -95,12 +150,12 @@
                 return false;
             });
 
-              $(window).scroll(function () {
-                    if (($(this).scrollTop() > 300)) {
-                        $('.scroller').fadeIn();
-                    } else {
-                        $('.scroller').fadeOut();
-                    }
+            $(window).scroll(function () {
+                if (($(this).scrollTop() > 300)) {
+                    $('.scroller').fadeIn();
+                } else {
+                    $('.scroller').fadeOut();
+                }
             });
         }
     };
--- sites/all/themes/mf/theme-settings.php
+++ sites/all/themes/mf/theme-settings.php
@@ -14,14 +14,4 @@ function mf_form_system_theme_settings_alter(&$form, $form_state) {
   // You can use this hook to append your own theme settings to the theme
   // settings form for your subtheme. You should also take a look at the
   // 'extensions' concept in the Omega base theme.
-
-//dpm($name_of_logo);
-//    dpm(theme_get_setting('logo_path'));
-
-//    $form ['logo']['settings']['logo_path'] = array(
-//        '#type' => 'textfield',
-//        '#title' => t('Path to custom logo'),
-//        '#description' => t('The path to the file you would like to use as your logo file instead of the default logo.'),
-//        '#default_value' => theme_get_setting('logo_path'),
-//    );
 }
