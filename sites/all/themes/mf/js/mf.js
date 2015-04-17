14c0f423aa9f3f7a421b0074c2615e6ca2759986
Konstantin Shupenko <koskinparkin@gmail.com>
2015-04-17 17:58:46 +0600

    123

--- sites/all/themes/mf/js/mf.js
+++ sites/all/themes/mf/js/mf.js
@@ -13,7 +13,37 @@
                 $("#cboxClose").css( {'opacity':'0.2', 'transition':'0.3s'});
             });
 
-            function margins($default_width, $default_height, relativeX, relativeY) {
+            var colorbox_info = function($amount_of_items) {
+                if ($amount_of_items) {
+                    $text = "Click anywhere on the image to zoom or click on zoom buttons on the bottom left side. Also you may click and drag the image to pan. If you want to change picture just click on the any image on the bottom right.";
+                }
+                else {
+                    $text = "Click anywhere on the image to zoom or click on zoom buttons on the left down side. Also you may click and drag the image to pan.";
+                }
+                return $text;
+            };
+
+            var check_size = function($element) {
+                $default_width = $('img', $element).attr('width');
+                $default_height = $('img', $element).attr('height');
+                var sizes = {};
+
+                if (($default_width > 960) || ($default_height > 620)) {
+                    $koef_sizes = $default_width/$default_height;
+                    $height = 620;
+                    $koef = $default_height/$height;
+                    $width = $default_width/$koef;
+                    sizes.width = $width;
+                    sizes.height = $height;
+                }
+                else {
+                    sizes.width = $default_width;
+                    sizes.height = $default_height;
+                }
+                return sizes;
+            };
+
+            var margins = function($default_width, $default_height, relativeX, relativeY) {
                 var margins = {};
                 var $half_width = $default_width/2;
                 var $half_height = $default_height/2;
@@ -34,21 +64,25 @@
                     margins.margin_top = $margin_vertical;
                 }
                 return margins;
-            }
+            };
 
             $('.node--product--colorbox', context).once('colorbox-imageslide', function() {
                 var $colorbox_node_wrapper = $(this);
-                $($colorbox_node_wrapper).append("<div class='change-image'><span>Zoom</span><span class='increase'></span><span class='decrease'></span></div>");
                 var $big_pictures_items = $('> .field--name-field-pictures .field__items', $colorbox_node_wrapper);
                 var $big_pictures = $('.field__item', $big_pictures_items);
+                var $text_for_colorbox_info = colorbox_info($big_pictures[1]);
                 var $thumbnails = $('> .field--name-one-more-picture .field--name-field-pictures .field__item', $colorbox_node_wrapper);
-                $($thumbnails).append("<div class='under-img'></div>");
 
-                $default_width = $('img', $big_pictures[0]).attr('width');
-                $default_height = $('img', $big_pictures[0]).attr('height');
+                $('> .field--name-one-more-picture', $colorbox_node_wrapper).before("<div class='colorbox-info'><span>" + $text_for_colorbox_info + "</span></div>");
+                $('> .field--name-one-more-picture', $colorbox_node_wrapper).before("<div class='change-image'><span>Zoom</span><span class='increase'></span><span class='decrease'></span></div>");
+                $thumbnails.append("<div class='under-img'></div>");
+
+
+                var $checked_size = check_size($big_pictures[0]);
                 var $checkpoint = false;
+                var $click_if = true;
                 $($big_pictures[0]).addClass('visible');
-                $('img', $big_pictures[0]).css({'transform': 'scale(1)', 'transition': '0.5s'});
+                $('img', $big_pictures[0]).css({'width': $checked_size['width'], 'height': $checked_size['height'], 'transition': '0.5s'});
                 $thumbnails.each(function(i) {
                     var $thumbnail = $(this);
                     $thumbnail.click(function() {
@@ -58,13 +92,13 @@
                                 $big_pictures_items.fadeOut(300, function () {
                                     $big_pictures.removeClass('visible');
                                     if ($('img', $big_pictures).hasClass('increased')) {
+                                        imgClickelse();
+                                        $click_if = true;
                                         $('img', $big_pictures).removeClass('increased');
                                     }
                                     $($big_pictures[i]).addClass('visible');
-                                    $change_imgclick = imgClickif;
-                                    $default_width = $('img', $big_pictures[i]).attr('width');
-                                    $default_height = $('img', $big_pictures[i]).attr('height');
-                                    $('img', $big_pictures[i]).css({'transform': 'scale(1)', 'transition': '0.5s'});
+                                    $checked_size = check_size($big_pictures[i]);
+                                    $('img', $big_pictures[i]).css({'width': $checked_size['width'], 'height': $checked_size['height'], 'transition': '0.5s'});
                                 });
                                 $big_pictures_items.fadeIn(300, function () {
                                     $checkpoint = false;
@@ -75,55 +109,53 @@
                     var imgClickif = function(relativeX, relativeY) {
                         if (!$('img', $big_pictures[i]).hasClass('increased')) {
                             $('img', $big_pictures[i]).addClass('increased');
-                          //  $new_sizes = resize($default_width, $default_height);
-                            $margins = margins($default_width, $default_height, relativeX, relativeY);
-
+                            $margins = margins($checked_size['width'], $checked_size['height'], relativeX, relativeY);
                             $('img', $big_pictures[i]).css({
-                                'margin-left': $margins['margin_left'],
+                                'transform': 'scale(2)',
                                 'margin-top': $margins['margin_top'],
+                                'margin-left': $margins['margin_left'],
                                 'margin-bottom': $margins['margin_bottom'],
                                 'margin-right': $margins['margin_right'],
-                                'transform': 'scale(2)',
-                                'transition': '0.5s'});
-
+                                'transition': '1s ease'});
                         }
                     };
                     var imgClickelse = function() {
-                        if ($('img', $big_pictures[i]).hasClass('increased')) {
-                            $('img', $big_pictures[i]).removeClass('increased');
-                            $('img', $big_pictures[i]).css({
+                        if ($('img', $big_pictures).hasClass('increased')) {
+                            $('img', $big_pictures).removeClass('increased');
+                            $('img', $big_pictures).css({
+                                'transform': 'scale(1)',
                                 'margin-left': '0',
                                 'margin-top': '0',
                                 'margin-right': '0',
                                 'margin-bottom': '0',
-                                'transform': 'scale(1)',
-                                'transition': '0.5s'});
+                                'transition': '1s ease'
+                            });
                         }
                     };
-                    var $change_imgclick = imgClickif;
+
+
                     $('img', $big_pictures[i]).click(function(e) {
-                        if ($change_imgclick == imgClickif) {
+                        if ($click_if ==  true) {
                             var offset = $('img', $big_pictures[i]).offset();
                             var relativeX = (e.pageX - offset.left);
                             var relativeY = (e.pageY - offset.top);
                             imgClickif(relativeX, relativeY);
-
-                            $change_imgclick = imgClickelse;
+                            $click_if = false;
                         }
                         else {
                             imgClickelse();
-                            $change_imgclick = imgClickif;
+                            $click_if = true;
                         }
                     });
 
 
                     $("> .change-image .increase", $colorbox_node_wrapper).click(function(){
                         imgClickif();
-                        $change_imgclick = imgClickelse;
+                        $click_if = false;
                     });
                     $("> .change-image .decrease", $colorbox_node_wrapper).click(function(){
                         imgClickelse();
-                        $change_imgclick = imgClickif;
+                        $click_if = true;
                     });
                 });
 
